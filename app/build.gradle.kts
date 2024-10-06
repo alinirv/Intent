34bbec0f569e9655073f4f842caf1ea8e9b13e0c
arv <alini.vicente@yahoo.com.br>
2024-10-06 14:39:55 -0300

    feat: enable View Binding and replace findViewById with binding in Activities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -32,6 +33,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
     }
 }
 
