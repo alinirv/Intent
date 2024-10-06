b09bad15f9debaa67b8a4b7f5edcccfc070181be
arv <alini.vicente@yahoo.com.br>
2024-10-06 16:19:20 -0300

    feat: handle parameter input and result in ParametroActivity

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/intent/ParametroActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/intent/ParametroActivity.kt
@@ -1,10 +1,12 @@
 package br.edu.ifsp.scl.ads.pdm.intent
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.pdm.intent.MainActivity.Constantes.PARAMETRO_EXTRA
 import br.edu.ifsp.scl.ads.pdm.intent.databinding.ActivityParametroBinding
 
 class ParametroActivity : AppCompatActivity() {
@@ -21,6 +23,20 @@ class ParametroActivity : AppCompatActivity() {
         supportActionBar?.apply {
             title= getString(R.string.app_name)
             subtitle=this@ParametroActivity.javaClass.canonicalName
+        }
+
+        intent.getStringExtra(PARAMETRO_EXTRA)?.also { parametro->
+            apb.parametroEt.setText(parametro)
+        }
+
+        apb.enviarParametroBt.setOnClickListener{
+            Intent().apply {
+                apb.parametroEt.text.toString().let {
+                    this.putExtra(PARAMETRO_EXTRA, it)
+                }
+                setResult(RESULT_OK,this)
+            }
+            finish()
         }
 
     }
